9f2233d111e9cee9eb2ccb30a2eb0d5d25cb3490
AbdullahFathan <abdullahfathan013@gmail.com>
2023-12-23 19:35:34 +0700

    feat: fix default back button

--- lib/screens/thrid_screen.dart
+++ lib/screens/thrid_screen.dart
@@ -44,69 +44,76 @@ class _ThirdScreenState extends State<ThirdScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBarWidget("Third Screen", context, result: name),
-        body: BlocConsumer<UserCubit, UserState>(
-          listener: (context, state) {
-            if (state is FetchUserEror) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text("Eror at get user")));
-            }
+        body: WillPopScope(
+          //handle default back button android so it can get selected user name
+          onWillPop: () async {
+            Navigator.pop(context, name);
+            return false;
           },
-          builder: (context, state) {
-            if (state is FetchUserLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (state is FetchUserNoData) {
-              return const Center(
-                child: Text("No Data"),
-              );
-            }
-            return RefreshIndicator(
-              onRefresh: () async {
-                //reset page number to initial value as [1]
-                page = 1;
-                context.read<UserCubit>().fetchUserData(page, true);
-              },
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView.separated(
-                      controller: _scrollController,
-                      itemCount: allUser.length,
-                      separatorBuilder: (context, index) {
-                        return const Divider(
-                          indent: 16,
-                          endIndent: 16,
-                          color: greyColor,
-                        );
-                      },
-                      itemBuilder: (context, index) {
-                        return UserCardWidget(
-                          userModel: allUser[index],
-                          onCallback: (String fullname) {
-                            setState(() {
-                              name = fullname;
-                            });
-                          },
-                        );
+          child: BlocConsumer<UserCubit, UserState>(
+            listener: (context, state) {
+              if (state is FetchUserEror) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Eror at get user")));
+              }
+            },
+            builder: (context, state) {
+              if (state is FetchUserLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is FetchUserNoData) {
+                return const Center(
+                  child: Text("No Data"),
+                );
+              }
+              return RefreshIndicator(
+                onRefresh: () async {
+                  //reset page number to initial value as [1]
+                  page = 1;
+                  context.read<UserCubit>().fetchUserData(page, true);
+                },
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.separated(
+                        controller: _scrollController,
+                        itemCount: allUser.length,
+                        separatorBuilder: (context, index) {
+                          return const Divider(
+                            indent: 16,
+                            endIndent: 16,
+                            color: greyColor,
+                          );
+                        },
+                        itemBuilder: (context, index) {
+                          return UserCardWidget(
+                            userModel: allUser[index],
+                            onCallback: (String fullname) {
+                              setState(() {
+                                name = fullname;
+                              });
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    BlocBuilder<UserCubit, UserState>(
+                      builder: (context, state) {
+                        if (state is FetchMoreUserLoading) {
+                          return const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 5),
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Container();
                       },
                     ),
-                  ),
-                  BlocBuilder<UserCubit, UserState>(
-                    builder: (context, state) {
-                      if (state is FetchMoreUserLoading) {
-                        return const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 5),
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      return Container();
-                    },
-                  ),
-                ],
-              ),
-            );
-          },
+                  ],
+                ),
+              );
+            },
+          ),
         ));
   }
 }
